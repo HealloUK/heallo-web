4b6c61fee3d688de931b8c86a51bc30105cc6a9f
HealloUk <109734631+HealloUk@users.noreply.github.com>
2022-07-25 17:32:23 +0300

    Tab Details

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -107,107 +107,31 @@ const CourseDetails = () => {
                   id="consider"
                   aria-labelledby="consider-tab"
                 >
-                  <h3 className="course-details__tab-title">
-                    Starting beginners level course
-                  </h3>
-                  <br />
-                  <p className="course-details__tab-text">
-                    Aelltes port lacus quis enim var sed efficitur turpis gilla
-                    sed sit Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry’s
-                    standard dummy text ever since.
-                  </p>
-                  <br />
                   <ul className="course-details__curriculum-list list-unstyled">
                     <li>
                       <div className="course-details__curriculum-list-left">
                         <div className="course-details__meta-icon video-icon">
-                          <i className="fas fa-play"></i>
                         </div>
-                        <a href="#none">Introduction to Editing</a>{" "}
-                        <span>Preview</span>
-                      </div>
-                      <div className="course-details__curriculum-list-right">
-                        16 minutes
+                        <a href="#none">Aiming for a better proportion and a more appealing look</a>{" "}
                       </div>
                     </li>
+                    
                     <li>
                       <div className="course-details__curriculum-list-left">
                         <div className="course-details__meta-icon video-icon">
-                          <i className="fas fa-play"></i>
                         </div>
-                        <a href="#none">Overview of Editing</a>{" "}
-                        <span>Preview</span>
-                      </div>
-                      <div className="course-details__curriculum-list-right">
-                        10 minutes
+                        <a href="#none">Restoring the symmetry of the breasts</a>{" "}
                       </div>
                     </li>
-                    <li>
-                      <div className="course-details__curriculum-list-left">
-                        <div className="course-details__meta-icon file-icon">
-                          <i className="fas fa-folder"></i>
-                        </div>
-                        <a href="#none">Basic Editing Technology</a>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="course-details__curriculum-list-left">
-                        <div className="course-details__meta-icon quiz-icon">
-                          <i className="fas fa-comment"></i>
-                        </div>
-                        <a href="#none">Quiz</a>
-                      </div>
-                      <div className="course-details__curriculum-list-right">
-                        6 questions
-                      </div>
-                    </li>
-                  </ul>
-                  <br />
-                  <br />
-                  <h3 className="course-details__tab-title">
-                    Intermediate Level
-                  </h3>
-                  <br />
-                  <p className="course-details__tab-text">
-                    Aelltes port lacus quis enim var sed efficitur turpis gilla
-                    sed sit Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry’s
-                    standard dummy text ever since.
-                  </p>
-                  <br />
-                  <ul className="course-details__curriculum-list list-unstyled">
+
                     <li>
                       <div className="course-details__curriculum-list-left">
                         <div className="course-details__meta-icon video-icon">
-                          <i className="fas fa-play"></i>
                         </div>
-                        <a href="#none">Introduction to Editing</a>
-                        <span>Preview</span>
-                      </div>
-                      <div className="course-details__curriculum-list-right">
-                        16 minutes
+                        <a href="#none">Post-pregnancy, massive weight loss and aging</a>{" "}
                       </div>
                     </li>
-                    <li>
-                      <div className="course-details__curriculum-list-left">
-                        <div className="course-details__meta-icon file-icon">
-                          <i className="fas fa-folder"></i>
-                        </div>
-                        <a href="#none">Basic Editing Technology</a>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="course-details__curriculum-list-left">
-                        <div className="course-details__meta-icon quiz-icon">
-                          <i className="fas fa-comment"></i>
-                        </div>
-                        <a href="#none">Quiz</a>
-                      </div>
-                      <div className="course-details__curriculum-list-right">
-                        6 questions
-                      </div>
-                    </li>
+                    
                   </ul>
                 </div>
 
@@ -218,175 +142,36 @@ const CourseDetails = () => {
                   id="expectations"
                   aria-labelledby="expectations-tab"
                 >
-                  <div className="row">
-                    <div className="col-xl-7 d-flex">
-                      <div className="course-details__progress my-auto">
-                        <div className="course-details__progress-item">
-                          <p className="course-details__progress-text">
-                            Excellent
-                          </p>
-                          <div className="course-details__progress-bar">
-                            <span style={{ width: `95%` }}></span>
-                          </div>
-                          <p className="course-details__progress-count">5</p>
+                  <ul className="course-details__curriculum-list list-unstyled">
+                    <li>
+                      <div className="course-details__curriculum-list-left">
+                        <div className="course-details__meta-icon video-icon">
                         </div>
-                        <div className="course-details__progress-item">
-                          <p className="course-details__progress-text">
-                            Very Good
-                          </p>
-                          <div className="course-details__progress-bar">
-                            <span style={{ width: `65%` }}></span>
-                          </div>
-                          <p className="course-details__progress-count">2</p>
-                        </div>
-                        <div className="course-details__progress-item">
-                          <p className="course-details__progress-text">
-                            Average
-                          </p>
-                          <div className="course-details__progress-bar">
-                            <span style={{ width: `33%` }}></span>
-                          </div>
-                          <p className="course-details__progress-count">1</p>
-                        </div>
-                        <div className="course-details__progress-item">
-                          <p className="course-details__progress-text">Poor</p>
-                          <div className="course-details__progress-bar">
-                            <span
-                              style={{ width: `0%` }}
-                              className="no-bubble"
-                            ></span>
-                          </div>
-                          <p className="course-details__progress-count">0</p>
-                        </div>
-                        <div className="course-details__progress-item">
-                          <p className="course-details__progress-text">
-                            Terrible
-                          </p>
-                          <div className="course-details__progress-bar">
-                            <span
-                              style={{ width: `0%` }}
-                              className="no-bubble"
-                            ></span>
-                          </div>
-                          <p className="course-details__progress-count">0</p>
-                        </div>
+                        <a href="#none">Improved balance, shape and symmetry</a>{" "}
                       </div>
-                    </div>
-                    <div className="col-xl-5 justify-content-xl-end justify-content-sm-center d-flex">
-                      <div className="course-details__review-box">
-                        <p className="course-details__review-count">4.6</p>
-                        <div className="course-details__review-stars">
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star-half"></i>
+                    </li>
+                    
+                    <li>
+                      <div className="course-details__curriculum-list-left">
+                        <div className="course-details__meta-icon video-icon">
                         </div>
-                        <p className="course-details__review-text">
-                          30 reviews
-                        </p>
+                        <a href="#none">Youthful appearance</a>{" "}
                       </div>
-                    </div>
-                  </div>
-                  <div className="course-details__comment">
-                    <div className="course-details__comment-single">
-                      <div className="course-details__comment-top">
-                        <div className="course-details__comment-img">
-                          <img src={team1} alt="" />
+                    </li>
+
+                    <li>
+                      <div className="course-details__curriculum-list-left">
+                        <div className="course-details__meta-icon video-icon">
                         </div>
-                        <div className="course-details__comment-right">
-                          <h2 className="course-details__comment-name">
-                            Steven Meyer
-                          </h2>
-                          <div className="course-details__comment-meta">
-                            <p className="course-details__comment-date">
-                              26 July, 2019
-                            </p>
-                            <div className="course-details__comment-stars">
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star star-disabled"></i>
-                            </div>
-                          </div>
-                        </div>
+                        <a href="#none">Enhanced self-confidence</a>{" "}
                       </div>
-                      <p className="course-details__comment-text">
-                        Lorem ipsum is simply free text used by copytyping
-                        refreshing. Neque porro est qui dolorem ipsum quia quaed
-                        inventore veritatis et quasi architecto beatae vitae
-                        dicta sunt explicabo.
-                      </p>
-                    </div>
-                    <div className="course-details__comment-single">
-                      <div className="course-details__comment-top">
-                        <div className="course-details__comment-img">
-                          <img src={team2} alt="" />
-                        </div>
-                        <div className="course-details__comment-right">
-                          <h2 className="course-details__comment-name">
-                            Lina Kelley
-                          </h2>
-                          <div className="course-details__comment-meta">
-                            <p className="course-details__comment-date">
-                              26 July, 2019
-                            </p>
-                            <div className="course-details__comment-stars">
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star star-disabled"></i>
-                              <i className="fa fa-star star-disabled"></i>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <p className="course-details__comment-text">
-                        Lorem ipsum is simply free text used by copytyping
-                        refreshing. Neque porro est qui dolorem ipsum quia quaed
-                        inventore veritatis et quasi architecto beatae vitae
-                        dicta sunt explicabo.
-                      </p>
-                    </div>
-                  </div>
-                  <form action="#" className="course-details__comment-form">
-                    <h2 className="course-details__title">Add a review</h2>
-                    <p className="course-details__comment-form-text">
-                      Rate this Course?{" "}
-                      <a href="#give-star" aria-label="review stars">
-                        <i className="fas fa-star"></i>
-                      </a>
-                      <a href="#give-star" aria-label="review stars">
-                        <i className="fas fa-star"></i>
-                      </a>
-                      <a href="#give-star" aria-label="review stars">
-                        <i className="fas fa-star"></i>
-                      </a>
-                      <a href="#give-star" aria-label="review stars">
-                        <i className="fas fa-star"></i>
-                      </a>
-                      <a href="#give-star" aria-label="review stars">
-                        <i className="fas fa-star"></i>
-                      </a>
-                    </p>
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <input type="text" placeholder="Your Name" />
-                        <input type="text" placeholder="Email Address" />
-                      </div>
-                      <div className="col-lg-12">
-                        <textarea placeholder="Write Message"></textarea>
-                        <button
-                          type="submit"
-                          className="thm-btn course-details__comment-form-btn"
-                        >
-                          Leave a Review
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                    </li>
+                    
+                  </ul>
                 </div>
+
+
+                
               </div>
 
             </div>
