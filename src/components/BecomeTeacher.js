0ceba684f4f8f20e5a961992c4fd6b57d19ba5aa
HealloUk <109734631+HealloUk@users.noreply.github.com>
2022-07-26 02:36:15 +0300

    Get quote

--- src/components/BecomeTeacher.js
+++ src/components/BecomeTeacher.js
@@ -2,48 +2,42 @@ import React from 'react';
 
 const BecomeTeacher = () => {
     return (
-        <section className="become-teacher">
+        <section className="get-quote">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6">
-                        <div className="become-teacher__content">
-                            <h2 className="become-teacher__title">Teaching benefits</h2>
-                            <p className="become-teacher__text">Lorem Ipsum is simply dummy text of the printing and
-                                type industry.
-                                Lorem Ipsum has been the standard dummy text ever since the when an unknown was
-                                popularised. It
-                                has survived not only five centuries, but also the leap into electronic typesetting
-                                remaining
-                                unchanged.</p>
-                            <h2 className="become-teacher__subtitle">Health and
-                                Pension</h2>
-                            <p className="become-teacher__text">Lorem Ipsum has been the standard dummy text ever since
-                                the when an
-                                unknown was popularised. It has survived not only five centuries. but also the leap into
-                                electronic typesetting remaining unchanged.</p>
-                            <h2 className="become-teacher__subtitle">Vacation
-                                Time</h2>
-                            <p className="become-teacher__text">Lorem Ipsum has been the standard dummy text ever since
-                                the when an
-                                unknown was popularised. It has survived not only five centuries. but also the leap into
-                                electronic typesetting remaining unchanged.</p>
+                        <div className="get-quote__content">
+                            <h2 className="get-quote__title">About Heallo</h2>
+                            <p className="get-quote__text">We connect patients our trusted doctors, clinics, hospitals. 
+                            We have a wide range of services and treatments for you to choose from all at an affordable price.</p>
+                            <h2 className="get-quote__subtitle">Experienced team of Professionals</h2>
+                            <p className="get-quote__text">We are dedicated to helping our clients look and feel their best. 
+Our experienced team of professionals will work with you to create a personalized treatment plan that meets your needs and fits your budget.</p>
+                            <h2 className="get-quote__subtitle">Perfect Journey</h2>
+                            <p className="get-quote__text">We design every step of your medical journey from the moment you 
+leave your home to the moment you return home.
+
+Our team is passionate about providing excellent customer service 
+and we are dedicated to ensuring that you have wonderful experience.
+
+Contact us today to schedule a consultation!</p>
                         </div>
                     </div>
                     <div className="col-lg-6">
-                        <div className="become-teacher__form">
-                            <div className="become-teacher__form-top">
-                                <h2 className="become-teacher__form-title">
+                        <div className="get-quote__form">
+                            <div className="get-quote__form-top">
+                                <h2 className="get-quote__form-title">
                                     Get Free Quote
                                 </h2>
                             </div>
                             <form action="#"
-                                  className="become-teacher__form-content contact-form-validated"
+                                  className="get-quote__form-content contact-form-validated"
                                   noValidate="novalidate">
                                 <input type="text" placeholder="Your Name" name="name" />
                                 <input type="text" placeholder="Email Address" name="email" />
                                 <input type="text" placeholder="Phone Number" name="phone" />
                                 <input type="text" placeholder="Comment" name="message" />
-                                <button type="submit" className="thm-btn become-teacher__form-btn">Apply
+                                <button type="submit" className="thm-btn get-quote__form-btn">Apply
                                     For It
                                 </button>
                             </form>
