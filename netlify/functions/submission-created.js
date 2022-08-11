26876f172ea29c0a6cbd5852ca5ea582afc7ec00
HealloUk <109734631+HealloUk@users.noreply.github.com>
2022-08-11 14:29:44 +0300

    Func

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,12 +1,54 @@
+const Mailgun = require('mailgun-js');
+
+const sendThankYouEmail = async ({ email }) => {
+  return new Promise((resolve, reject) => {
+    console.log('Sending the email');
+    const { MG_API_KEY: apiKey, MG_DOMAIN: domain } = process.env;
+    const mailgun = Mailgun({
+      apiKey,
+      domain
+    });
+
+    const mailData = {
+      from: 'Stefan Judis <no-reply@stefanjudis.com>',
+      to: email,
+      subject: 'Thank you for your interest',
+      text: "I'll come back to you asap!"
+    };
+
+    mailgun.messages().send(mailData, err => {
+      if (err) return reject(err);
+
+      resolve();
+    });
+  });
+};
+
+
 module.exports.handler = async function(event, context) {
-  const data = JSON.parse(event.body)
-  console.log(data)
-  return {
-    // return null to show no errors
-    statusCode: 200, // http status code
-    body: JSON.stringify({
-      msg: "Mail sent"
-    })
+
+  try {
+	  const data = JSON.parse(event.body)
+    
+    await sendThankYouEmail(data);
+
+	  console.log(data)
+	  return {
+	    // return null to show no errors
+	    statusCode: 200, // http status code
+	    body: JSON.stringify({
+	      msg: "Mail sent"
+	    })
+	  }
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: e.message
+    }
   }
+
+  
+
 }
 
